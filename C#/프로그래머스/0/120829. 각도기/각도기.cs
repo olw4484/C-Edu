357d1eb7963d1c2191e87cbc1f336892b5d12c00
owl4484 <rodeo23267286@gmail.com>
2025-03-14 16:58:09 +0900

    [level 0] Title: 각도기, Time: 0.25 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120829. 각도기/각도기.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Solution {
+    public int solution(int angle) {
+        if (angle >= 0 && angle < 90)
+        {
+            return 1;
+        }
+        else if(angle == 90)
+        {
+            return 2;
+        }
+        else if ( angle >= 91 && angle <180)
+        {
+            return 3;
+        }
+        else
+            return 4;
+    }
+}
