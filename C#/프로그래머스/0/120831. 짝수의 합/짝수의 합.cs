27edaec8ae598995a0655a0c667467f4cd6582fd
owl4484 <rodeo23267286@gmail.com>
2025-03-14 17:15:46 +0900

    [level 0] Title: 짝수의 합, Time: 0.25 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120831. 짝수의 합/짝수의 합.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int sum = 0;
+        for (int i = 0; i <= n; i += 2)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+}
