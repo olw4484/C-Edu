9914253990288336054b6d2e9382585fa21f0fec
owl4484 <rodeo23267286@gmail.com>
2025-03-16 12:49:19 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.18 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+        int evenCount = 0;
+        int oddCount = 0;
+        for (int i = 0; i < num_list.Length; i++)
+        {
+            if (num_list[i] % 2 == 0)
+            {
+                evenCount++;
+            }
+            else
+            {
+                oddCount++;
+            }
+            
+        }
+
+        
+        return new int[] {evenCount, oddCount} ;
+    }
+}
