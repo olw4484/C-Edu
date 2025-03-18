11bd6d0c69458a1b0136a7530fd3355ee1305bfd
owl4484 <rodeo23267286@gmail.com>
2025-03-19 00:45:36 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.31 ms, Memory: 31.6 MB -BaekjoonHub

--- C#/프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.cs
+++ C#/프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.cs
@@ -2,16 +2,14 @@ using System;
 
 public class Solution {
     public double solution(double n) {
-        int count = 0;
         double sqrt = Math.Sqrt(n);
         if (n % sqrt == 0)
         {
-            count++;
+            return 1;
         }
         else
         {
             return 2;
         }
-        return count;
     }
 }
