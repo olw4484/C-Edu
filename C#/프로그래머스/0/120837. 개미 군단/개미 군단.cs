488d78e7a5f5ab113b8f402b1320fefb914228fd
owl4484 <rodeo23267286@gmail.com>
2025-03-29 15:07:18 +0900

    [level 0] Title: 개미 군단, Time: 0.17 ms, Memory: 31.3 MB -BaekjoonHub

--- C#/프로그래머스/0/120837. 개미 군단/개미 군단.cs
+++ C#/프로그래머스/0/120837. 개미 군단/개미 군단.cs
@@ -4,17 +4,14 @@ public class Solution {
     public int solution(int hp) {
         int antG = 5;
         int antS = 3;
-        int antW = 1;
         int answer = 0;
         
         answer += hp / antG;
         hp %= antG;
         answer += hp / antS;
         hp %= antS;
-        answer += hp / antW;
-        hp %= antW;
+        answer += hp / 1;
         
-
         return answer;
     }
 }
